65d310f1c88b62bb584a13fdf982c026086118b0
shark <771279601@qq.com>
2018-10-18 14:38:47 +0800

    项目初始化，第一次提交

---
+++ src/components/Child.vue
@@ -0,0 +1,57 @@
+<template>
+    <div>
+        <h1> {{ msg }} </h1>
+        <button type="button" class="mui-btn mui-btn-success">绿色</button>
+        <mt-popup v-model="popupVisible" position="right">右侧弹出</mt-popup>
+        <mt-swipe :auto="4000">
+            <mt-swipe-item>
+                <mt-button icon="more" type="default" @click="popupVisible = true">更多</mt-button>
+            </mt-swipe-item>
+            <mt-swipe-item>
+                <mt-button type="primary" @click="toast">primary</mt-button>
+            </mt-swipe-item>
+            <mt-swipe-item>
+                <mt-button type="danger" @click="indicator">danger</mt-button>
+            </mt-swipe-item>
+        </mt-swipe>
+    </div>
+</template>
+
+<script>
+import { Toast, Indicator, MessageBox } from "mint-ui";
+export default {
+  name: "Child",
+  data() {
+    return {
+      msg: "web前端19期",
+      popupVisible:false
+    };
+  },
+  methods: {
+    toast() {
+      Toast({
+        message: "提示",
+        position: "bottom",
+        duration: 5000
+      });
+    },
+    indicator() {
+      Indicator.open({
+        text: "加载中...",
+        spinnerType: "fading-circle"
+      });
+      setTimeout(function(){
+          Indicator.close();
+      },700)
+    }
+  }
+};
+</script>
+
+<style scoped>
+div {
+  color: chartreuse;
+  background-color: slateblue;
+  height: 200px;
+}
+</style>
---
+++ src/components/Test.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        {{msg}}
+        <router-view></router-view>
+    </div>
+</template>
+<script>
+    export default {
+        name:'Test',
+        data() {
+            return {
+                msg: '黑马程序员'
+            }
+        }
+    }
+    export var a = '测试';
+</script>
+<style scoped>
+    div {
+        font-style: italic;
+    }
+</style>
